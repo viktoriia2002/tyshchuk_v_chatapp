b19193afbdd50b5fe9154b141743c75a12820e82
macbook <macbook@macbooks-MacBook-Pro.local>
2021-02-08 18:27:46 -0500

    chat app start

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const messenger = require('socket.io')();
+
+const app = express();
+
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+       res.sendFile(path.join(__dirname, "index.html")); //localhost:3000/index.html);
+});
+
+app.get("/chat", (req, res) => {
+       res.sendFile(path.join(__dirname, "chat.html")); //localhost:3000/index.html);
+});
+
+const server = app.listen(port, () => {
+       console.log(`app is running on ${port}`);
+});
+
+
+messenger.attach(server);
